d64241096110f2c5e9d01abbfd303e82ab1e6866
rmbls <139141589+rmbls@users.noreply.github.com>
2025-04-30 16:50:43 -0400

    Update setup to include numpy and jaraco

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 APP = ['Lagence Project.py']
 OPTIONS = {
     'argv_emulation': True,
-    'packages': ['numpy', 'pandas'],
+    'includes': ['numpy', 'pandas', 'jaraco.text'],
     'excludes': [
         'PyInstaller', 'PySide2', 'PySide6', 'packaging', 'backports', 'backports.tarfile',
         'tkinter', '_tkinter'
@@ -14,5 +14,5 @@ setup(
     app=APP,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
-    install_requires=['jaraco.text'],
+    install_requires=['jaraco.text', 'numpy', 'pandas'],
 )
